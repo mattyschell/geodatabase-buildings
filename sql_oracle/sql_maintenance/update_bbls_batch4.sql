893939866d2488d97498e84dd748d26c5de4edc2
Matt Schell <mschell@doitt.nyc.gov>
2022-12-30 10:51:07 -0500

    adds batch 4 of bad bbl corrections

---
+++ sql_oracle/sql_maintenance/update_bbls_batch4.sql
@@ -0,0 +1,98 @@
+
+--manually create badbbls version under edit version
+
+call sde.version_util.set_current_version('badbbls');
+
+--start editing
+call sde.version_user_ddl.edit_version ('badbbls', 1);
+
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3056330034 where doitt_id = 570754;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4161560153 where doitt_id = 572810;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4101150053 where doitt_id = 575335;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4002650013 where doitt_id = 580315;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3004270007 where doitt_id = 580880;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3012690007 where doitt_id = 581521;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2032150044 where doitt_id = 582801;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1009390024 where doitt_id = 582861;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3056510034 where doitt_id = 583634;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2041780018 where doitt_id = 585344;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5008540084 where doitt_id = 586106;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 586214;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2037860001 where doitt_id = 588511;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 588949;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3023260029 where doitt_id = 589607;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1015480023 where doitt_id = 591352;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1014910021 where doitt_id = 591893;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 592913;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4161560135 where doitt_id = 595840;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 597109;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1007670067 where doitt_id = 598945;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3004530031 where doitt_id = 600259;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2041390106 where doitt_id = 602739;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2027770230 where doitt_id = 603408;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 605982;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3002780024 where doitt_id = 607670;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1002320023 where doitt_id = 608528;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2033180139 where doitt_id = 608910;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 609296;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3053460015 where doitt_id = 610343;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3054350044 where doitt_id = 610479;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2031240046 where doitt_id = 610752;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2024230038 where doitt_id = 613365;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 619089;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 627596;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3071590037 where doitt_id = 635444;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 640346;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3052980027 where doitt_id = 641008;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 641815;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 655076;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 657710;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 659236;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 665187;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 665393;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 673599;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 678308;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 689261;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 691504;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 692284;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 726781;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 729602;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 739577;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 743752;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 751471;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 753259;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 753490;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 759564;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 779589;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 816313;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 819319;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 820584;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 821193;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 847284;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 847331;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 1010597;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 1294277;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 5024500539 where doitt_id = 1294278;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4096570001 where doitt_id = 643940;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3088561584 where doitt_id = 646413;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4161560146 where doitt_id = 651049;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1015030044 where doitt_id = 652430;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4161560138 where doitt_id = 653128;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3076620017 where doitt_id = 655972;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3034990025 where doitt_id = 656253;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3039890001 where doitt_id = 657860;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1020320030 where doitt_id = 658083;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4101150053 where doitt_id = 659352;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3060350046 where doitt_id = 661442;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 3006550031 where doitt_id = 666633;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2028520036 where doitt_id = 668084;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 4018560079 where doitt_id = 670424;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1010070005 where doitt_id = 674373;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 2023140067 where doitt_id = 675002;
+update bldg.building_evw set mappluto_bbl = NULL, condo_flags = NULL, base_bbl = 1006260031 where doitt_id = 675195;
+
+commit;
+
+call sde.version_user_ddl.edit_version ('badbbls', 2);
+
+--reconcile and post and delete badbbls 
