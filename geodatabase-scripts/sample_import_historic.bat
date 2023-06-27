8fd00e7077cdd1f188b213f639a4e33661a8f850
Matt Schell <mschell@doitt.nyc.gov>
2023-06-27 12:23:28 -0400

    adds qa to building_historic import sample

--- geodatabase-scripts/sample_import_historic.bat
+++ geodatabase-scripts/sample_import_historic.bat
@@ -1,49 +1,45 @@
-REM change these
-set DBENV=DEV
-set DBNAME=XXXXXXXXX
-set BASEPATH=X:\XXX
-set SOURCEFC=%BASEPATH%\connections\oracle19c\XXX\GIS-XXXXXXXX\bldg.sde\BLDG.BUILDING_HISTORIC
-set EDITORS=XXXXXX,XXXXXX
-REM unmask these
-set NOTIFY=XXXXXX@XXX.nyc.gov
-set NOTIFYFROM=XXXXXX@XXX.nyc.gov
-set SMTPFROM=XXXXXXXXX.XXXXXX
-REM review these 
-set PY27=C:\Python27\ArcGIS10.7\python.exe
-set SDEFILE=%BASEPATH%\connections\oracle19c\%DBENV%\GIS-%DBNAME%\bldg.sde
-REM feel, gather, and realize these. Silencio
-set VIEWERS=BLDG_READONLY
+set SDEFILE=X:\XXX\xxxx\xxxx\xxx\xxxx\xxx.sde
 set TARGETFC=BUILDING_HISTORIC
-set TARGETLOGDIR=%BASEPATH%\geodatabase-scripts\logs\building_historic_import\
-set TOILER=%BASEPATH%\geodatabase-toiler\
-set BUILDINGS=%BASEPATH%\geodatabase-buildings\
+set SOURCEFC=X:\XXX\xxxx\xxxx\xxx\xxxx\xxx.sde\BLDG.BUILDING_HISTORIC
+set EDITORS=xxxx,xxxx
+set VIEWERS=xxxx
+set NOTIFY=xxx@xxx.xxx.xxx
+set NOTIFYFROM=xxx@xx.xx.xxx
+set SMTPFROM=xxx.xxx
+set TARGETLOGDIR=x:\xxx\geodatabase-scripts\logs\building_historic_import\
+set TOILER=X:\xxx\geodatabase-toiler\
+set BUILDINGS=X:\xxx\geodatabase-buildings\
 set PYTHONPATH=%TOILER%\src\py;%BUILDINGS%
 set PROPY=c:\Progra~1\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe
-set BATLOG=%TARGETLOGDIR%building_historic_import.log
-echo starting building import to target %TARGETFC% on %date% at %time% > %TARGETLOGDIR%building_historic_import.log
+set PY27=C:\Python27\ArcGIS10.7\python.exe
+echo starting building import to target %TARGETFC% on %date% at %time% > %TARGETLOGDIR%building_import.log
 %PROPY% %BUILDINGS%delete.py %TARGETFC% && (
-  echo. deleted target %TARGETFC% on %date% at %time% >> %BATLOG%
+  echo. >> deleted target %TARGETFC% on %date% at %time% > %TARGETLOGDIR%building_historic_import.log
 ) || (
   %PROPY% %BUILDINGS%notify.py ": Failed to delete %TARGETFC% on %SDEFILE%" %NOTIFY% "building_historic_import" && EXIT /B 1
 )  
 %PROPY% %BUILDINGS%import.py %TARGETFC% %SOURCEFC% && (
-  echo. >> %BATLOG% && echo imported %TARGETFC% on %date% at %time% >> %BATLOG%
+  echo. >> imported %TARGETFC% on %date% at %time% >> %TARGETLOGDIR%building_historic_import.log
 ) || (
   %PROPY% %BUILDINGS%notify.py ": Failed to import %TARGETFC% on %SDEFILE%" %NOTIFY% "building_historic_import" && EXIT /B 1
 )  
 %PROPY% %BUILDINGS%grant.py %TARGETFC% %EDITORS% "N" && (
-  echo. >> %BATLOG% && echo granted %TARGETFC% edit privileges on %date% at %time% >> %BATLOG%
+  echo. >> granted %TARGETFC% edit privileges on %date% at %time% >> %TARGETLOGDIR%building_historic_import.log
 ) || (
   %PROPY% %BUILDINGS%notify.py ": Failed to grant %TARGETFC% edit privileges on %SDEFILE%" %NOTIFY% "building_historic_import" && EXIT /B 1
 )  
 %PROPY% %BUILDINGS%grant.py %TARGETFC% %VIEWERS% "Y" && (
-  echo. >> %BATLOG% && echo granted %TARGETFC% viewing privileges on %date% at %time% >> %BATLOG%
+  echo. >> granted %TARGETFC% viewing privileges on %date% at %time% >> %TARGETLOGDIR%building_historic_import.log
 ) || (
   %PROPY% %BUILDINGS%notify.py ": Failed to grant %TARGETFC% viewing privileges on %SDEFILE%" %NOTIFY% "building_historic_import" && EXIT /B 1
-)  
+)   
 %PY27% %TOILER%\src\py27\create_versionedviews.py %TARGETFC% && (
-  echo. >> %BATLOG% && echo created versioned view %TARGETFC%_EVW on %date% at %time% >> %BATLOG%
+  echo. >> created versioned view for %TARGETFC% on %date% at %time% >> %TARGETLOGDIR%building_historic_import.log
 ) || (
   %PROPY% %BUILDINGS%notify.py ": Failed to create versioned view for %TARGETFC% on %SDEFILE%" %NOTIFY% "building_historic_import" && EXIT /B 1
 ) 
-%PROPY% %BUILDINGS%notify.py ": Successfully imported %TARGETFC% to %SDEFILE%" %NOTIFY% "building_historic_import"
+%PROPY% %BUILDINGS%qa.py %TARGETFC% "shape" && (
+  %PROPY% %BUILDINGS%notify.py ": Successfully imported %TARGETFC% to %SDEFILE%" %NOTIFY% "building_historic_import"
+) || (
+  %PROPY% %BUILDINGS%notify.py ": Failed QA of imported %TARGETFC% on %SDEFILE%" %NOTIFY% "qa"
+) 
