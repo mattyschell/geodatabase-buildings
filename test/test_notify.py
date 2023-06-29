b55b6080f5d74502b5c7493674fc21d8bef35423
Matt Schell <mschell@doitt.nyc.gov>
2023-06-29 09:18:46 -0400

    adds some poor tests for notify log checks

--- test/test_notify.py
+++ test/test_notify.py
@@ -20,10 +20,44 @@ class NotifyTestCase(unittest.TestCase):
 
         try:
             # refactor cluephone - system call 
+            # sends an email as a test. come on now
+            # we are merely testing that we don't error 
+            # and demonstrating usages. better than nothing i guess
             os.system('{0} notify.py {1} {2} {3}'.format(self.propy
                                                         ,'testing-email-from-' + self.computername   
                                                         ,self.notifyto
                                                         ,'test'))
+        except:
+            raise ValueError('notify.py call failed')
+
+        pass
+
+    def test_bnotifycheck(self):
+
+        #check the log for caminante and notify 
+
+        try:
+            os.system('{0} notify.py {1} {2} {3} {4}'.format(self.propy
+                                                            ,'testing-email-from-' + self.computername   
+                                                            ,self.notifyto
+                                                            ,'test'
+                                                            ,'caminante'))
+        except:
+            raise ValueError('notify.py call failed')
+
+        pass
+
+    def test_cnonotifycheck(self):
+
+        # check the log for a word that is not there
+        # should not notify but you must test with eyeballs
+
+        try:
+            os.system('{0} notify.py {1} {2} {3} {4}'.format(self.propy
+                                                            ,'testing-email-from-' + self.computername   
+                                                            ,self.notifyto
+                                                            ,'test'
+                                                            ,'antoniomachado'))
         except:
             raise ValueError('notify.py call failed')
 
