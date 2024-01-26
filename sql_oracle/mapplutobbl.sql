e9866bda56e70806a5f9d7cc4f0e1b15893e98ec
Matt Schell <mschell@doitt.nyc.gov>
2024-01-26 12:45:01 -0500

    decondofies mappluto bbl resolves #51

--- sql_oracle/mapplutobbl.sql
+++ sql_oracle/mapplutobbl.sql
@@ -8,6 +8,10 @@ BEGIN
     -- Run before nightly reconcile and post
     sde.version_util.set_current_version(editversion);
     sde.version_user_ddl.edit_version(editversion,1);
+
+    -- this sets condos regardless of what an editor does
+    -- any base_bbl that has a special mappluto_bbl on the list
+    -- will get one applied here
     psql := 'update '
          || '    ' || featureclass || '_evw a '
          || 'set '
@@ -30,8 +34,9 @@ BEGIN
     -- click save
     commit;
 
-    -- everything not a condo from above
-    -- copy base_bbl to mappluto_bbl
+    -- everything not a condo copy base_bbl to mappluto_bbl
+    -- this only applies to null mappluto_bbl
+    -- (usually new buildings) 
     psql := 'update '
          || '   ' || featureclass || '_evw a '
          || 'set '
@@ -41,6 +46,25 @@ BEGIN
     execute immediate psql;
     commit;
 
+    -- occasionally editors will mistakenly set mappluto_bbl to be
+    -- different from base_bbl 
+    -- or a building base_bbl will stop being a condo (alteration i guess)
+    -- or we will update the base_bbl to a lot that isnt a condo (ex lot split)
+    -- flip these back to not being condo bbls as they arise
+    -- https://github.com/mattyschell/geodatabase-buildings/issues/51
+    psql := 'update '
+         || '    ' || featureclass || '_evw a '
+         || 'set '
+         || '    a.mappluto_bbl = a.base_bbl '
+         || 'where '
+         || '    a.base_bbl <> a.mappluto_bbl '
+         || 'and a.base_bbl not in (select '
+         || '                           b.condo_base_bbl '
+         || '                       from '
+         || '                           ' || condotable || ' b )';
+    execute immediate psql;
+    commit;
+    
     -- stop editing
     sde.version_user_ddl.edit_version(editversion,2);
 
