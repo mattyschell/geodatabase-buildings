547953eee9e77f4f15aef7b12d4d470ea7f47d86
Matt Schell <mschell@doitt.nyc.gov>
2025-01-03 10:31:09 -0500

    nulls se_anno_cad_data in remove curves

--- sql_oracle/compileremovecurves.sql
+++ sql_oracle/compileremovecurves.sql
@@ -11,10 +11,13 @@ begin
     -- remove_curves is intended for use in import.py
     -- we will execute this procedure against the base table before versioning
     -- if running after versioning pass in xxx_evw as the table name 
+    -- must null the secret hieroglyphics in se_anno_cad_data to 
+    --    prevent the curves from re-appearing in future edits
 
     psql := 'update ' || p_table_name || ' a ' 
          || 'set '
-         || '   a.shape = sdo_geom.sdo_arc_densify(a.shape, :p1, :p2) ' 
+         || '   a.shape = sdo_geom.sdo_arc_densify(a.shape, :p1, :p2) '
+         || '  ,a.se_anno_cad_data = NULL ' 
          || 'where '
          || 'a.' || p_pkc_col || ' in ( '
          || '   select distinct ' || p_pkc_col || ' ' 
